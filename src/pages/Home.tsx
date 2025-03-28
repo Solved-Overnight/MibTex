daba6f0c072a636a391f0ee922dd6c61a1ee3a24
Solved-Overnight <mzrishad.info@gmail.com>
2025-03-28 09:16:41 +0600

    version 04

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,15 +7,16 @@ import ImageSlider from '../components/ImageSlider';
 // IMPORTANT: Create a 'public/images' folder and place your images there.
 // Then update these paths accordingly. For example: '/images/factory.jpg'
 const storyImages = [
-  'img\img1.jpeg',
-  'img\img2.jpeg',
-  'img\img3.jpeg',
-  'img\img4.jpeg',
-  'img\img5.jpeg',
-  'img\img6.jpeg',
-  'img\img7.jpeg',
-  'img\img8.jpeg',
-  'img\img9.jpeg',
+  '/img/img1.jpeg',
+  '/img/img2.jpg',
+  '/img/img3.jpeg',
+  '/img/img4.jpg',
+  '/img/img5.jpeg',
+  '/img/img6.jpg',
+  '/img/img7.jpg',
+  '/img/img8.jpeg',
+  '/img/img9.jpeg',
+  '/img/img10.jpeg',
 ];
 
 const categoryImages = {
